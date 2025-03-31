dbf56304cc22cd4ed824fadb69deeee102de2def
Hebert Duarte <165860490+Hebert-12@users.noreply.github.com>
2025-03-31 20:52:53 -0300

    Add files via upload

--- src/pages/livrosDoados/livrosDoados.jsx
+++ src/pages/livrosDoados/livrosDoados.jsx
@@ -1,19 +1,39 @@
 import S from "./livrosDoados.module.scss"
 import Livro1 from "../../assets/img/livroDoado1.png"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 export default function LivrosDoados(){
+
+    const [livros, setLivros] = useState ([])
+
+    const getLivros = async ()=> {
+        const response = await axios.get("https://api-desafio-4zdr.onrender.com/livrosDoados")
+        setLivros(response.data)
+    }
+
+    useEffect (()=>{
+        getLivros()
+},[])
+
     return(
         <section className={S.Geral}>
             <h2>Livros Doados</h2>
             <section className={S.Galeria}>
                 <article>
                     <img src={Livro1} alt="Imagem do livro protagonista" />
-                    <div>
-                        <p>O protagonista</p>
-                        <p>Susanne Andrade</p>
-                        <p>Ficção</p>
-                    </div>
+                    <p>O protagonista</p>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
                 </article>
+                {livros.map((item)=>(
+                    <article key={item.id}>
+                        <img src={item.imagem_url} alt=""/>
+                        <h3>{item.titulo}</h3>
+                        <p>{item.categoria}</p>
+                        <p>{item.autor}</p>
+                    </article>
+                ))}
             </section>
         </section>
     )
